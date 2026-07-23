e7757935493f59e38adba1c695c912735cafdf00
ConnorWarren <connorqwarren@gmail.com>
2026-07-22 21:17:28 -0400

    Add backward pass

--- nn.py
+++ nn.py
@@ -2,76 +2,62 @@ import numpy as np
 import numpy.typing as npt
 from dataclasses import dataclass
 
-from functions import relu, softmax, relu_prime, softmax_prime, cel_prime
-
-class LinearLayer:
-    def __init__(self, in_size: int, out_size: int) -> None:
-        self.in_size = in_size
-        self.out_size = out_size
-        self.weights = np.random.rand(out_size, in_size)
-        self.biases = np.random.rand(out_size)
-
-    def forward(self, x: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
-        if np.size(x) != self.in_size:
-            raise Exception("Forward input must match input size")
-
-        return np.linalg.matmul(self.weights, x) + self.biases
+from functions import relu, relu_prime, softmax, cross_entropy_loss
 
 @dataclass
 class LayerCache:
     z: npt.NDArray[np.float64]
     a: npt.NDArray[np.float64]
 
+class Layer:
+    def __init__(self, in_size: int, out_size: int, activation_func) -> None:
+        self.cache = None
+        self.in_size = in_size
+        self.out_size = out_size
+        self.activation_func = activation_func
+        self.weights = np.random.rand(out_size, in_size) * 0.01
+        self.biases = np.random.rand(out_size) * 0.01
+
+    def forward(self, x: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+        if np.size(x) != self.in_size:
+            raise Exception("Forward input must match input size")
+        
+        z = self.weights @ x + self.biases
+        a = self.activation_func(z)
+        self.cache = LayerCache(z, a)
+        return a
+
 class NeuralNetwork:
-    def __init__(self):
+    def __init__(self) -> None:
         self.lr = 0.001
-        self.cache: list[LayerCache] = []
-        self.fc1 = LinearLayer(28*28, 128)
-        self.fc2 = LinearLayer(128, 64)
-        self.fc3 = LinearLayer(64, 10)
+        self.layers = [
+            Layer(28*28, 128, relu),
+            Layer(128, 64, relu),
+            Layer(64, 10, softmax)
+        ]
+        self.loss_func = cross_entropy_loss
+        self.input_layer = None
 
     def forward(self, x) -> npt.NDArray[np.float64]:
-        self.cache.append(LayerCache(x, x))
-        
-        z = self.fc1.forward(x)
-        a = relu(z)
-        self.cache.append(LayerCache(z, a))
+        self.input_layer = x
+        for layer in self.layers:
+            x = layer.forward(x)
 
-        z = self.fc2.forward(a)
-        a = relu(z)
-        self.cache.append(LayerCache(z, a))
-        
-        z = self.fc3.forward(a)
-        a = softmax(z)
-        self.cache.append(LayerCache(z, a))
-        
-        return a
-    
-    def clear_cache(self):
-        self.cache = []
+        return x
 
-    def backward(self, expected, actual):
-        loss_w3 = np.outer(cel_prime(expected, actual) \
-            * softmax_prime(self.cache[3].a), self.cache[2].a)
-      
-        loss_w2 = np.outer(loss_w3 * relu_prime(self.cache[2].a), self.cache[1].a)
+    def backward(self, prediction, label):
+        if not all([layer.cache for layer in self.layers]):
+            raise Exception("Run forward pass before calling backward")
 
-        loss_w1 = np.outer(loss_w2 * relu_prime(self.cache[1].a), self.cache[0].a)
-        
-        self.fc3.weights -= loss_w3 * self.lr
-        self.fc2.weights -= loss_w2 * self.lr
-        self.fc1.weights -= loss_w1 * self.lr
+        err_sig3 = prediction - label
+        grad_w3 = np.outer(err_sig3, self.layers[1].cache.a) # type: ignore
 
-        """del_z3 = softmax_prime(self.cache[3]) * relu_prime(self.cache[2]) \
-            * relu_prime(self.cache[1]) * self.cache[0]
-        grad_3 = loss_derivative(expected, actual) * del_z3
-        self.fc3.weights -= grad_3 * self.lr
-        
-        del_z2 = relu_prime(self.cache[2]) * relu_prime(self.cache[1]) * self.cache[0]
-        grad_2 = loss_derivative(expected, actual) * del_z2
-        self.fc2.weights -= grad_2 * self.lr
+        err_sig2 = err_sig3 @ self.layers[2].weights * relu_prime(self.layers[1].cache.z)
+        grad_w2 = np.outer(err_sig2, self.layers[0].cache.a) #type: ignore
 
-        del_z1 = relu_prime(self.cache[1]) * self.cache[0]
-        grad_1 = loss_derivative(expected, actual) * del_z1
-        self.fc1.weights -= grad_1 * self.lr
-        """
+        err_sig1 = err_sig2 @ self.layers[1].weights * relu_prime(self.layers[0].cache.z)
+        grad_w1 = np.outer(err_sig1, self.input_layer)
+
+        self.layers[2].weights -= self.lr * grad_w3
+        self.layers[1].weights -= self.lr * grad_w2
+        self.layers[0].weights -= self.lr * grad_w1
