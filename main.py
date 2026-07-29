640bb22b66585c9d66578869c7d684810c2c62c2
ConnorWarren <connorqwarren@gmail.com>
2026-07-29 13:33:33 -0400

    Change to mini-batch gradient descent

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
 import struct
 import numpy as np
 import numpy.typing as npt
+import matplotlib
+import matplotlib.pyplot as plt
+
 from nn import NeuralNetwork
+from functions import cross_entropy_loss
 
 def parse_mnist(images_path: str, labels_path: str) \
-    -> tuple[npt.NDArray[np.int8], npt.NDArray[np.int8]]:
+    -> tuple[npt.NDArray[np.float64], npt.NDArray[np.uint8]]:
     with open(images_path, "rb") as f:
         _, n_items, _, _ = struct.unpack(">iiii", f.read(16))
-        img_arr = np.frombuffer(f.read(), dtype=np.int8)
+        img_arr = np.frombuffer(f.read(), dtype=np.uint8).astype(np.float64) / 255
         img_matrix = img_arr.reshape(n_items, 28*28)
 
     with open(labels_path, "rb") as f:
         header = f.read(8)
         _, n_items = struct.unpack(">ii", header)
         digits = f.read(n_items)
-        label_arr = np.frombuffer(digits, dtype=np.int8)
+        label_arr = np.frombuffer(digits, dtype=np.uint8)
 
     return img_matrix, label_arr
 
-def train(model: NeuralNetwork, images, labels, epochs: int) -> None:
+def train(model: NeuralNetwork, images, labels, epochs: int, plot_loss=True) -> None:
+    batch_size = 256
     for epoch in range(epochs):
         print(f"Training epoch: {epoch}")
-        for image, label in zip(images, labels):
-            prediction = model.forward(image)
-            expected_arr = np.array([1 if n == label else 0 for n in range(10)])
-            model.backward(prediction, expected_arr)
+        batch = np.random.choice(len(images), size=batch_size, replace=False)
+        raw_predictions = model.forward(images[batch])
+        expected = np.zeros((batch_size, 10))
+        expected[range(batch_size), labels[batch]] = 1
+        model.backward(raw_predictions, expected)
 
 def test(model: NeuralNetwork, images, labels) -> None:
     total_correct = 0
     total_incorrect = 0
     for image, label in zip(images, labels):
-        prediction = np.argmax(model.forward(image))
+        prediction = np.argmax(model.forward(image[np.newaxis, :]))
         if prediction == label:
             total_correct += 1
         else:
@@ -45,7 +51,7 @@ def main() -> None:
         "train-images.idx3-ubyte",
         "train-labels.idx1-ubyte"
     )
-    train(model, images, labels, 2)
+    train(model, images, labels, 5000, plot_loss=False)
 
     test_images, test_labels = parse_mnist(
         "t10k-images.idx3-ubyte",
--- nn.py
+++ nn.py
@@ -15,27 +15,23 @@ class Layer:
         self.in_size = in_size
         self.out_size = out_size
         self.activation_func = activation_func
-        self.weights = np.random.rand(out_size, in_size) * 0.01
-        self.biases = np.random.rand(out_size) * 0.01
+        self.weights = np.random.randn(out_size, in_size) * 0.01
+        self.biases = np.random.randn(out_size) * 0.01
 
     def forward(self, x: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
-        if np.size(x) != self.in_size:
-            raise Exception("Forward input must match input size")
-        
-        z = self.weights @ x + self.biases
+        z = x @ self.weights.T + self.biases
         a = self.activation_func(z)
         self.cache = LayerCache(z, a)
         return a
 
 class NeuralNetwork:
     def __init__(self) -> None:
-        self.lr = 0.001
+        self.lr = .1
         self.layers = [
             Layer(28*28, 128, relu),
             Layer(128, 64, relu),
             Layer(64, 10, softmax)
         ]
-        self.loss_func = cross_entropy_loss
         self.input_layer = None
 
     def forward(self, x) -> npt.NDArray[np.float64]:
@@ -46,18 +42,20 @@ class NeuralNetwork:
         return x
 
     def backward(self, prediction, label):
-        if not all([layer.cache for layer in self.layers]):
+        if self.input_layer is None or not all([layer.cache for layer in self.layers]):
             raise Exception("Run forward pass before calling backward")
 
-        err_sig3 = prediction - label
-        grad_w3 = np.outer(err_sig3, self.layers[1].cache.a) # type: ignore
+        dZ_3 = prediction - label
+        dW_3 = dZ_3.T @ self.layers[1].cache.a # type: ignore
 
-        err_sig2 = err_sig3 @ self.layers[2].weights * relu_prime(self.layers[1].cache.z)
-        grad_w2 = np.outer(err_sig2, self.layers[0].cache.a) #type: ignore
+        dZ_2 = dZ_3 @ self.layers[2].weights * relu_prime(self.layers[1].cache.z)
+        dW_2 = dZ_2.T @ self.layers[0].cache.a #type: ignore
 
-        err_sig1 = err_sig2 @ self.layers[1].weights * relu_prime(self.layers[0].cache.z)
-        grad_w1 = np.outer(err_sig1, self.input_layer)
+        dZ_1 = dZ_2 @ self.layers[1].weights * relu_prime(self.layers[0].cache.z)
+        dW_1 = dZ_1.T @ self.input_layer
 
-        self.layers[2].weights -= self.lr * grad_w3
-        self.layers[1].weights -= self.lr * grad_w2
-        self.layers[0].weights -= self.lr * grad_w1
+        batch_size = self.input_layer.shape[0]
+
+        self.layers[2].weights -= self.lr * dW_3 / batch_size
+        self.layers[1].weights -= self.lr * dW_2 / batch_size
+        self.layers[0].weights -= self.lr * dW_1 / batch_size
