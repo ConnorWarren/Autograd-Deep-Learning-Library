b14a4cb518bbae0604ed453fb682d3b09b1c5b6f
ConnorWarren <connorqwarren@gmail.com>
2026-07-31 00:11:49 -0400

    Add command line arguments

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import struct
-import numpy as np
-import numpy.typing as npt
+import argparse
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
+import numpy.typing as npt
+import struct
 
 from nn import NeuralNetwork
-from functions import cross_entropy_loss
 
 def parse_mnist(images_path: str, labels_path: str) \
     -> tuple[npt.NDArray[np.float64], npt.NDArray[np.uint8]]:
@@ -27,8 +27,8 @@ def train(model: NeuralNetwork, images, labels, epochs: int, plot_loss=True) -> 
     rng = np.random.default_rng()
     for epoch in range(epochs):
         print(f"Training epoch: {epoch}")
+        indices = rng.permutation(len(images))
         for i in range(len(images) // batch_size):
-            indices = rng.permutation(len(images))
             batch_indices = indices[i*batch_size:(i+1)*batch_size]
             raw_predictions = model.forward(images[batch_indices])
             expected = np.zeros((batch_size, 10))
@@ -47,19 +47,39 @@ def test(model: NeuralNetwork, images, labels) -> None:
 
     print(f"Accuracy: {total_correct/(total_correct + total_incorrect) * 100}%")
 
-def main() -> None:    
-    model = NeuralNetwork() 
-    images, labels = parse_mnist(
-        "train-images.idx3-ubyte",
-        "train-labels.idx1-ubyte"
-    )
-    train(model, images, labels, 50, plot_loss=False)
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", default="model.npz", help="path to load/save trained model")
+    parser.add_argument("--train", action="store_true", help="Train and save model")
+    parser.add_argument("--load", action="store_true", help="Load model from file")
+    parser.add_argument("--test", action="store_true", help="Test the loaded/trained model")
+    parser.add_argument("--ui", action="store_true", help="Test the model with a drawing UI")
+    parser.add_argument("--plot", action="store_true", help="Plot loss function from training")
+    parser.add_argument("--epochs", type=int, default=20, help="Number of training epochs")
+    args = parser.parse_args()
 
-    test_images, test_labels = parse_mnist(
-        "t10k-images.idx3-ubyte",
-        "t10k-labels.idx1-ubyte"
-    )
-    test(model, test_images, test_labels)
+    model = NeuralNetwork()
+
+    if args.load:
+        model.load(args.path)
+
+    if args.train:
+        images, labels = parse_mnist(
+            "train-images.idx3-ubyte",
+            "train-labels.idx1-ubyte"
+        )
+        train(model, images, labels, args.epochs, plot_loss=False)
+        model.save(args.path)
+
+    if args.test:
+        test_images, test_labels = parse_mnist(
+            "t10k-images.idx3-ubyte",
+            "t10k-labels.idx1-ubyte"
+        )
+        test(model, test_images, test_labels)
+
+    if args.ui:
+        ...
 
 if __name__ == "__main__":
     main()
