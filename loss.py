4bc809f4ac8fa51d79d54dbfe9a7bf9d91da8f4b
ConnorWarren <connorqwarren@gmail.com>
2026-08-31 20:26:39 -0400

    Add loss.py

---
+++ loss.py
@@ -0,0 +1,16 @@
+import numpy as np
+import numpy.typing as npt
+from tensor import Tensor
+from functions import cross_entropy_loss, softmax
+
+def cel_softmax(expected: Tensor, predicted: Tensor) -> Tensor:
+    probs = softmax(predicted.array)
+    output = Tensor(
+        np.array(cross_entropy_loss(expected.array, probs)),
+        inputs=(expected, predicted)
+    )
+    def backward():
+        predicted.gradient = predicted.array - expected.array
+
+    output.backward = backward
+    return output
