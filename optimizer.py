b4fda8c887c510d65c7a502ea5b451fa75639509
ConnorWarren <connorqwarren@gmail.com>
2026-09-05 13:48:22 -0400

    Move zero_gradients to optimizer

--- optimizer.py
+++ optimizer.py
@@ -1,6 +1,7 @@
 from tensor import Parameter
 
 class SGD:
+    """Optimizer for batch stochastic gradient descent."""
     def __init__(self, parameters: list[Parameter], learning_rate: float, batch_size):
         self.parameters = parameters
         self.learning_rate = learning_rate
@@ -9,3 +10,7 @@ class SGD:
     def step(self):
         for param in self.parameters:
             param.array = param.array - self.learning_rate * param.gradient / self.batch_size
+    
+    def zero_gradients(self):
+        for param in self.parameters:
+            param.zero_gradient()
