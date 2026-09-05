Add __repr__ method, gradient setter, and fix backward computations in __add__ and __matmul__

tensor.py
@@ -5,22 +5,23 @@ from functions import relu, relu_prime
 
 class Tensor:
     """Represents a tensor node in a computational graph."""
-    def __init__(self, array: npt.NDArray, inputs: tuple[Tensor, ...] = (), label=""):
+    def __init__(self, array: npt.NDArray, inputs: tuple["Tensor", ...] = (), label=""):
         self.array = array
         self.inputs = inputs
-        self.gradient: npt.NDArray = np.zeros(self.array.shape)
+        self._gradient: npt.NDArray = np.zeros_like(self.array)
         self.backward = None
         self.label = label
 
     def __repr__(self):
-        return f"Tensor({self.label})"
+        return f"Tensor({self.label + "," if self.label else ""}" \
+              f"{"x".join([str(dim) for dim in self.array.shape])})"
 
     def __add__(self, other: "Tensor") -> "Tensor":
         output = Tensor(self.array + other.array, (self, other))
 
         def backward():
-            self.gradient = self.gradient + output.gradient
-            other.gradient = other.gradient + output.gradient
+            self.gradient += output.gradient
+            other.gradient += np.sum(output.gradient, axis=0)
 
         output.backward = backward
 
@@ -41,13 +42,25 @@ class Tensor:
         output = Tensor(self.array @ other.array, (self, other))
 
         def backward():
-            self.gradient = self.gradient + output.gradient @ other.array.T
-            other.gradient = other.gradient.T + output.gradient.T @ self.array
+            self.gradient += output.gradient @ other.array.T
+            other.gradient += self.array.T @ output.gradient
 
         output.backward = backward
-        
+    
         return output
     
+    @property
+    def gradient(self):
+        return self._gradient
+    
+    @gradient.setter
+    def gradient(self, new_grad: npt.NDArray):
+        if new_grad.shape != self.array.shape:
+            raise Exception(f"Gradient shape {new_grad.shape} does not match tensor shape {self.array.shape}")
+    
+    def zero_gradient(self) -> None:
+        self.gradient = np.zeros_like(self.array)
+
     def sum(self) -> "Tensor":
         output = Tensor(self.array.sum(), inputs=(self,))
 
@@ -71,17 +84,11 @@ class Tensor:
         output = Tensor(self.array.T, inputs=(self, ))
 
         def backward():
-            self.gradient = self.array.T
+            self.gradient = output.gradient.T
 
         self.backward = backward
 
         return output
-
-    def zero_gradients(self) -> None:
-        """Zero all gradients in the computational graph."""
-        self.gradient = np.zeros(self.array.shape)
-        for tensor in self.inputs:
-            tensor.zero_gradients()
 
     def topological_sort(self) -> list["Tensor"]:
         """Generate a valid toplogical ordering of the computational graph."""
@@ -110,7 +117,7 @@ class Tensor:
         if self.array.size != 1:
             raise Exception("Backward can only be called on a scalar valued tensor.")
         
-        self.gradient = np.ones_like(self.array.shape)
+        self.gradient = np.ones_like(self.array)
         for tensor in reversed(self.topological_sort()[1:]):
             if tensor.backward is None:
                 continue